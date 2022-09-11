384659887a53a328b7713fa88596fded89ba466d
Desiders <47452083+Desiders@users.noreply.github.com>
2022-09-11 15:30:26 +0300

    ✨ Add a better check of the blacklisted URL
    Now there isn't check for complete similarity, but for starts with.

--- src/parser.rs
+++ src/parser.rs
@@ -58,7 +58,10 @@ fn get_href<'a>(
                     return Some(href);
                 }
             }
-        } else if !blacklist_hrefs.contains(&strip_suffix_slash(href)) {
+        } else {
+            if value_in_blacklist(href, blacklist_hrefs) {
+                return None;
+            }
             return Some(href);
         }
     }
@@ -120,36 +123,12 @@ fn get_href_media_type<'a>(href: &str, media_type_or_domain_match: &'a Match) ->
 }
 
 pub fn get_url(parent_url: &str, href: &str, blacklist_urls: &[String]) -> Option<String> {
-    lazy_static! {
-        static ref ARGUMENTS: Regex = Regex::new(r"(\?\S*)").unwrap(); // (\?\S*)
-    }
-
     let url = get_url_from_href(parent_url, href);
 
-    if let Some(cap) = ARGUMENTS.captures(href) {
-        let arguments = cap.get(1).unwrap().as_str();
-        let url_without_args = url.replace(arguments, "");
-
-        if !blacklist_urls.contains(&strip_suffix_slash(&url_without_args)) {
-            Some(url)
-        } else {
-            None
-        }
+    if value_in_blacklist(&url, blacklist_urls) {
+        None
     } else {
-        if !blacklist_urls.contains(&strip_suffix_slash(&url)) {
-            Some(url)
-        } else {
-            None
-        }
-    }
-}
-
-// for proper verification in the blacklist
-fn strip_suffix_slash(value: &str) -> String {
-    if value.ends_with('/') {
-        value.rsplit_once('/').unwrap().0.to_string()
-    } else {
-        value.to_string()
+        Some(url)
     }
 }
 
@@ -177,5 +156,41 @@ fn concat_url_with_href(url: &str, href: &str) -> String {
             unimplemented!();
         }
         format!("{}{}", url, href)
+    }
+}
+
+fn value_in_blacklist(value: &str, blacklist: &[String]) -> bool {
+    lazy_static! {
+        static ref ARGUMENTS: Regex = Regex::new(r"(\?\S*)").unwrap(); // (\?\S*)
+    }
+
+    if let Some(cap) = ARGUMENTS.captures(value) {
+        let arguments = cap.get(1).unwrap().as_str();
+        let value_without_args = value.replace(arguments, "");
+
+        let value_stripped = strip_value_after_slash(&value_without_args);
+        for blacklist_value in blacklist {
+            if value_stripped.starts_with(blacklist_value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    let value_stripped = strip_value_after_slash(value);
+    for blacklist_value in blacklist {
+        if value_stripped.starts_with(blacklist_value) {
+            return true;
+        }
+    }
+    false
+}
+
+// for proper verification in the blacklist
+fn strip_value_after_slash(value: &str) -> String {
+    if value.ends_with('/') {
+        value.rsplit_once('/').unwrap().0.to_string()
+    } else {
+        value.to_string()
     }
 }
