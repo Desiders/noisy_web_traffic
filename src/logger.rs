2a3f1301e11e50abe8e309ddbb9a3c31478a8f00
Desiders <47452083+Desiders@users.noreply.github.com>
2022-09-16 18:36:36 +0300

    🔊 Update logger initialization
    Added standard logging level

--- src/logger.rs
+++ src/logger.rs
@@ -1,3 +1,13 @@
+use env_logger::{Builder, Env};
+
+static DEFAULT_FILTER: &str = "info";
+
 pub fn init() {
-    env_logger::init();
+    Builder::from_env(Env::default().default_filter_or(DEFAULT_FILTER))
+        .format_level(true)
+        .format_module_path(true)
+        .format_target(false)
+        .format_indent(None)
+        .format_timestamp_secs()
+        .init();
 }
