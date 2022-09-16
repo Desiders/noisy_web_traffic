30a02be9d4419c3323abd44bdca91db3714f2ca3
Desiders <47452083+Desiders@users.noreply.github.com>
2022-09-16 18:33:04 +0300

    ➕ New yaml parser

--- src/config_reader.rs
+++ src/config_reader.rs
@@ -1,6 +1,7 @@
-use std::{error::Error, fs::File, io::Read, path::Path};
-use yaml_rust::YamlLoader;
+use serde_derive::{Deserialize, Serialize};
+use std::{fs::File, io::Read, path::Path};
 
+#[derive(Serialize, Deserialize)]
 pub struct Client {
     pub max_depth: u32,
     pub min_sleep: u32,
@@ -10,26 +11,31 @@ pub struct Client {
     pub max_failures: u32,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct BlacklistUrls {
     pub childs: Vec<String>,
     pub hrefs: Vec<String>,
     pub types: Vec<String>,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct Urls {
     pub roots: Vec<String>,
     pub blacklist: BlacklistUrls,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct UserAgent {
     pub generate: bool,
     pub current: Option<String>,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct MachineConfig {
     pub name: String,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct Config {
     pub client: Client,
     pub urls: Urls,
@@ -37,86 +43,13 @@ pub struct Config {
     pub machine_config: MachineConfig,
 }
 
-pub fn parse_config(path: &str) -> Result<Config, Box<dyn Error>> {
+pub fn parse_config(path: &str) -> serde_yaml::Result<Config> {
     let mut contents = String::new();
 
-    File::open(&Path::new(path))?.read_to_string(&mut contents)?;
+    File::open(Path::new(path))
+        .expect("Failed to open config")
+        .read_to_string(&mut contents)
+        .expect("Failed to read config");
 
-    let docs = YamlLoader::load_from_str(&contents)?;
-    let doc = &docs[0];
-
-    let client = {
-        let client = &doc["client"];
-        let max_depth = client["max_depth"].as_i64().unwrap().try_into()?;
-        let min_sleep = client["min_sleep"].as_i64().unwrap().try_into()?;
-        let max_sleep = client["max_sleep"].as_i64().unwrap().try_into()?;
-        let max_timeout = client["max_timeout"].as_i64().unwrap().try_into()?;
-        let max_redirections = client["max_redirections"].as_i64().unwrap().try_into()?;
-        let max_failures = client["max_failures"].as_i64().unwrap().try_into()?;
-        Client {
-            max_depth,
-            min_sleep,
-            max_sleep,
-            max_timeout,
-            max_redirections,
-            max_failures,
-        }
-    };
-
-    let urls = {
-        let urls = &doc["urls"];
-        let roots = urls["roots"]
-            .as_vec()
-            .unwrap_or(&Vec::new())
-            .iter()
-            .map(|url| url.as_str().unwrap().to_string())
-            .collect();
-        let blacklist = {
-            let blacklist = &urls["blacklist"];
-            let childs = blacklist["childs"]
-                .as_vec()
-                .unwrap_or(&Vec::new())
-                .iter()
-                .map(|url| url.as_str().unwrap().to_string())
-                .collect();
-            let hrefs = blacklist["hrefs"]
-                .as_vec()
-                .unwrap_or(&Vec::new())
-                .iter()
-                .map(|url| url.as_str().unwrap().to_string())
-                .collect();
-            let types = blacklist["types"]
-                .as_vec()
-                .unwrap_or(&Vec::new())
-                .iter()
-                .map(|url| url.as_str().unwrap().to_string())
-                .collect();
-            BlacklistUrls {
-                childs,
-                hrefs,
-                types,
-            }
-        };
-        Urls { roots, blacklist }
-    };
-
-    let user_agent = {
-        let user_agent = &doc["user_agent"];
-        let generate = user_agent["generate"].as_bool().unwrap();
-        let current = user_agent["current"].as_str().map(ToString::to_string);
-        UserAgent { generate, current }
-    };
-
-    let machine_config = {
-        let machine_config = &doc["machine_config"];
-        let name = machine_config["name"].as_str().unwrap().to_string();
-        MachineConfig { name }
-    };
-
-    Ok(Config {
-        client,
-        urls,
-        user_agent,
-        machine_config,
-    })
+    serde_yaml::from_str::<Config>(&contents)
 }
